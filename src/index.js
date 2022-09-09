e9fe2e9e108af5d1aebb0106b8e8e70ede4771e9
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2022-09-09 22:28:42 +0200

    next steps

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader';
+import * as THREE from 'three';
+
+export const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+document.body.appendChild(renderer.domElement);
+
+scene.background = new THREE.Color(0x000000);
 console.log('begin')
