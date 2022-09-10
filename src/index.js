389001a43d8c42ad51cc8d342e2cbd1765d499b0
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2022-09-10 15:36:04 +0200

    lights added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader';
 import * as THREE from 'three';
+import { light2, light3, light4 } from './direct_light.js';
 
 export const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -10,4 +11,12 @@ renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 scene.background = new THREE.Color(0x000000);
+
+scene.add(light2);
+scene.add(light2.target);
+scene.add(light3);
+scene.add(light3.target);
+scene.add(light4);
+scene.add(light4.target);
+
 console.log('begin')
