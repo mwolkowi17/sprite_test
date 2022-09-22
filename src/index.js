180acd077a66376cf45f93302e17375a1ed1fe7f
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2022-09-22 14:56:27 +0200

    orbit control added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader';
 import * as THREE from 'three';
+import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls';
 import { light2, light3, light4 } from './direct_light.js';
 import {cube} from './box.js'
 
@@ -22,7 +23,8 @@ scene.add(light4.target);
 scene.add(cube);
 
 camera.position.z = 5;
-
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
 
 console.log('begin')
 
