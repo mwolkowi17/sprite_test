5bc5d9f54105a9897acd9cbc0aecdee6dc8591d3
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2022-09-22 07:40:31 +0200

    basic setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader';
 import * as THREE from 'three';
 import { light2, light3, light4 } from './direct_light.js';
+import {cube} from './box.js'
 
 export const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -18,5 +19,21 @@ scene.add(light3);
 scene.add(light3.target);
 scene.add(light4);
 scene.add(light4.target);
+scene.add(cube);
+
+camera.position.z = 5;
+
 
 console.log('begin')
+
+const animate = function () {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.02;
+    cube.rotation.y += 0.01;
+
+
+    renderer.render(scene, camera);
+}
+
+animate();
