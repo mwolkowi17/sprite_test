259ea081f31ad3a8be81fab3d9d27e797b2e3af1
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2022-09-23 19:58:10 +0200

    gltf traversing added

--- src/loader.js
+++ src/loader.js
@@ -4,10 +4,22 @@ import { scene } from './index.js';
 
 const gltfLoader2 = new GLTFLoader()
 
-    gltfLoader2.load('./zawor_kulowy_three_kula3_korpus.glb', (gltf) => {
+    gltfLoader2.load('./sprite1.gltf', (gltf) => {
       const root = gltf.scene;
       
       scene.add(root); //co zrobic z tym?
+      
+      const sceneobjects = [];
+
+      gltf.scene.traverse(function (child) {
+        if (child.isMesh) {
+          const m = child;
+          m.castShadow = true;
+          sceneobjects.push(m);
+          //originalMaterials[m.name] = (m).material;
+        }
+      });
+      console.log(sceneobjects);
       
      
       
